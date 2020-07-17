ed724500dd874d451fcd12521f3f8c5762069041
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-17 18:54:52 -0500

    images and data.js updated

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,11 +1,33 @@
 const galleryItems = [
   {
     id: 1,
-    path: 'images/goat_small.jpg',
+    title: 'Goatster',
     description: 'Photo of a goat taken at Glacier National Park.',
+    path: 'images/goat_small.jpg',
     likes: 0,
   },
-  
+  {
+    id: 2,
+    title: 'Butterfly Hand',
+    description: 'Photo of a butterfly in a hand.',
+    path: 'images/butterfly_hand.png',
+    likes: 0,
+  },
+  {
+    id: 3,
+    title: 'Ambiance With Friends',
+    description: 'Photo of a ambiance and friends.',
+    path: 'images/friends_table.png',
+    likes: 0,
+  },
+
+  {
+    id: 4,
+    title: 'Kind Humans',
+    description: 'Be a kind human',
+    path: 'images/kind_human.png',
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
