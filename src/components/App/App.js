65650ffca6856f0e7aaddbb4389eacc191c5a8f7
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-17 20:31:59 -0500

    set state in app, brought in axios, and mounted componeet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import GalleryList from '../GalleryList/GalleryList';
 
 class App extends Component {
+  state = {
+    galleryItems: [
+      {
+        id: 0,
+        title: '',
+        description: '',
+        path: '',
+        likes: 0,
+      },
+    ],
+  };
+
+  componentDidMount() {
+    console.log('We did da Mount!');
+    //   this.getData();
+  }
   render() {
     return (
       <div className="App">
@@ -12,7 +29,7 @@ class App extends Component {
         <br />
         <p>Gallery goes here</p>
         <GalleryList />
-        <img src="images/goat_small.jpg" alt="this is an" />
+        {/* <img src="images/goat_small.jpg" alt="this is an" /> */}
       </div>
     );
   }
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 class GalleryList extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <GalleryItem />
+        <GalleryItem />
+        <GalleryItem />
+        <GalleryItem />
+        <GalleryItem />
+        <GalleryItem />
+      </div>
+    );
   }
 }
 
