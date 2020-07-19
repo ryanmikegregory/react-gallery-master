b77f4651ea9c080fad380aca9b347cc377955b37
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-19 11:14:41 -0500

    styled iages inline-block and removed bullets from li's

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
+import styles from '../GalleryList/GalleryList.module.css';
 
 export class GalleryList extends Component {
   render() {
     const itemsArray = this.props.galleryList.map((item, index) => {
       return (
-          <div key={index}>
+        <div key={index} className={styles.box}>
           <li key={index}>
-          <img key={index} src={item.path} alt="pic from array"></img>
+            <img key={index} src={item.path} alt="pic from array"></img>
           </li>
-          </div>
-        
+        </div>
       );
     });
-    return (
-      <div>
-        {itemsArray}
-      </div>
-    );
+    return <div>{itemsArray}</div>;
   }
 }
 
 export default GalleryList;
-
