0af3e6dbc15fbb255436353589233453500e4b67
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-18 23:31:53 -0500

    Created style module

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import styles from '../GalleryItem/GalleryItem.module.css';
 
 export class GalleryItem extends Component {
   render() {
     console.log(this.props.galleryItem);
     return (
-      <div>
+      <div className={styles.children}>
         <h1>Test</h1>
         <img src={this.props.galleryItem.path} alt="arrayImage" />
-        <div><button>I Like It!</button></div>
+        <div>
+          <button>I Like It!</button>
+        </div>
       </div>
     );
   }
 }
 
 export default GalleryItem;
-
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import GalleryItem from '../GalleryItem/GalleryItem';
+import styles from '../GalleryList/GalleryList.module.css';
 
 class GalleryList extends Component {
   render() {
     return this.props.galleryList.map((galleryItem) => (
-      <div>
+      <div className={styles.box}>
         <GalleryItem galleryItem={galleryItem} />
       </div>
     ));
