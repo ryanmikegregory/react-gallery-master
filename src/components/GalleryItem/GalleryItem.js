c85e8d8c448f5aa03c258d583d6a72481255e27b
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-19 19:18:01 -0500

    added ALT to image

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './GalleryItem.css';
-
+// import Button from 'react-bootstrap/Button';
 
 class GalleryItem extends Component {
   render() {
     return (
       <div className="box">
         <h4>{this.props.item.title}</h4>
-        
-          <img src={this.props.item.path}></img>
-          
+
+        <img src={this.props.item.path} alt={this.props.item.description}></img>
 
         <p>{this.props.item.description}</p>
         <div>
